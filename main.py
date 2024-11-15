c3d42132caf48d98662a55423e382846ee935e0a
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-11-15 17:53:32 +0000

    a

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# Developed by Dadmehr Emami at home - Github @BDadmehr0/emamidadmehr@gmail.com
+
+"""
+MIT License
+
+Copyright (c) 2024 Dadmehr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import json  # Import json module to save player data
 import os
 import random
@@ -30,14 +57,9 @@ attack_message_shown = False  # To make sure message is only shown once
 
 # Allowed keys: only 'w', 'a', 's', 'd', and control keys like Ctrl+Z, Ctrl+C
 ALLOWED_KEYS = {
-    "w",
-    "a",
-    "s",
+    "a",  # Movement keys
     "d",
     "e",  # Key for attacking
-    keyboard.Key.ctrl_l,
-    keyboard.Key.ctrl_r,
-    keyboard.KeyCode.from_char("x"),
     keyboard.Key.shift_l,  # Left shift
     keyboard.Key.shift_r,  # Right shift
 }
